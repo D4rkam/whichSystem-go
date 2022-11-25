15070393016ba23d1e466128c7f3047766c42299
Thomas <thomaslinares32@gmail.com>
2022-11-25 19:05:54 -0300

    v1.0

--- main.go
+++ main.go
@@ -1,40 +1,89 @@
 package main
 
 import (
-	"fmt"
+	"flag"
 	"os"
 	"os/exec"
 	"regexp"
 	"runtime"
+	"strconv"
+	"strings"
+
+	"github.com/fatih/color"
 )
 
 func check(err error) {
 	if err != nil {
-		fmt.Print("A ocurrido un error:", err)
+		color.Red("[!] A ocurrido un error:", err)
 		os.Exit(1)
 	}
 }
 
-func windows(ip_address string) {
-	out, err := exec.Command("ping", ip_address).Output()
-	check(err)
-	output_command := string(out[:])
-	fmt.Println(output_command)
-	re := regexp.MustCompile(`Respuesta desde 181.85.123.83: bytes=32 tiempo<1m TTL=64 
-								Respuesta desde 181.85.123.83: bytes=32 tiempo=1ms TTL=64
-								Respuesta desde 181.85.123.83: bytes=32 tiempo<1m TTL=64 
-								Respuesta desde 181.85.123.83: bytes=32 tiempo=1ms TTL=64`)
-	ttl := re.FindStringSubmatch("TLL=64")
-	check(err)
-	fmt.Println("La salida del comando es:", ttl[0])
+func get_ttl(output_ping string) {
+	pattern := ` \w\w\w\=\d\d\d?`
+	delimitador := "="
+	const WINDOWS_TTL = 128
+	const LINUX_TTL = 64
 
+	re := regexp.MustCompile(pattern)
+	ttl_array := re.FindStringSubmatch(output_ping)
+
+	if ttl_array != nil {
+
+		color.Green("[+] Respondio")
+		ttl, err := strconv.Atoi(strings.Split(ttl_array[0], delimitador)[1])
+		check(err)
+		if ttl == LINUX_TTL {
+			color.Green("[+] El ttl es: %v --> linux", ttl)
+			os.Exit(0)
+		}
+		if ttl == WINDOWS_TTL {
+			color.Green("[+] El ttl es: %v --> windows", ttl)
+			os.Exit(0)
+		} else {
+			color.Yellow("[-] El ttl es: %v --> Sistema Operativo desconocido", ttl)
+			os.Exit(0)
+		}
+	}
+	color.Red("[!] La maquina no respondio")
+	os.Exit(1)
+}
+
+func windows(ip_address string) {
+	out, err := exec.Command("C:/Windows/System32/ping", ip_address).Output()
+	check(err)
+	output_ping := string(out[:])
+	get_ttl(output_ping)
+}
+
+func linux(ip_address string) {
+	out, err := exec.Command("/usr/bin/ping", ip_address).Output()
+	check(err)
+	output_ping := string(out[:])
+	get_ttl(output_ping)
+}
+
+func verify_os(ip_address string) {
+
+	if runtime.GOOS == "windows" {
+		color.Green("[+] Ejecutando ping en windows...")
+		windows(ip_address)
+		return
+	}
+	if runtime.GOOS == "linux" {
+		color.Green("[+] Ejecutando ping en linux...")
+		linux(ip_address)
+		return
+	} else {
+		color.Red("[-] Sistema Operativo no apto")
+		os.Exit(1)
+	}
 }
 
 func main() {
-	var ip_address string
-	fmt.Print("Ingrese la direccion ip:")
-	fmt.Scan(&ip_address)
-	if runtime.GOOS == "windows" {
-		windows(ip_address)
-	}
+	ip_address_flag := flag.String("ip", "", "Direccion IP")
+	flag.Parse()
+	ip_address := *ip_address_flag
+	color.Yellow("[*] Verificando tu Sistema Operativo...")
+	verify_os(ip_address)
 }
